ae9408726b05834a4008c5f0bc267a75ddcc5432
dennisjoseph2025-dotcom <dennisjoseph2025@gmail.com>
2025-11-04 20:57:10 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
     return (
         <nav className="fixed top-0 left-0 w-full h-16 bg-sky-900 shadow-lg border-b border-sky-800 flex items-center px-4 sm:px-8 z-30">
             
-            {/* Left: Nav Links */}
+            {/*  Nav Links */}
             <div className="flex flex-1 items-center justify-start">
                 <Menuebar/>
                 <div className="hidden sm:flex space-x-8 text-white font-semibold text-lg items-center">
@@ -31,7 +31,7 @@ function Navbar() {
                 </div>
             </div>
             
-            {/* Center: Logo */}
+            {/*  Logo */}
             <div className="absolute left-1/2 top-0 h-full flex items-center -translate-x-1/2">
                 <Link
                     to="/"
@@ -43,26 +43,18 @@ function Navbar() {
                 </Link>
             </div>
             
-            {/* Right: Icons */}
+            {/* Icons */}
             <div className="flex flex-1 items-center justify-end space-x-4 ml-4">
-                {/* <NavLink
-                    to="/search"
-                    className="text-white hover:text-blue-300 transition cursor-pointer text-2xl p-2 rounded-full"
-                    aria-label="Search"
-                >
-                    <CgSearch />
-                </NavLink> */}
                 <NavLink
                     to="/cart"
-                    className="relative p-2 rounded-full bg-blue-800 hover:bg-blue-700 transition cursor-pointer text-white text-2xl"
+                    className="relative p-2 rounded-full   cursor-pointer text-white text-2xl"
                     aria-label="Cart"
                 >
                     <RiShoppingCart2Line />
-                    {/* <span className="absolute -top-1 -right-1 bg-pink-500 text-white text-xs px-2 py-0.5 rounded-full font-bold">3</span> */}
                 </NavLink>
                 <NavLink
                     to={CkUser?.name ? "/userprofile" : "/login"}
-                    className="p-2 rounded-full bg-blue-800 hover:bg-blue-700 transition cursor-pointer text-white text-2xl"
+                    className="p-2 rounded-full  transition cursor-pointer text-white text-2xl"
                     aria-label="User Profile"
                 >
                     <CgProfile />
